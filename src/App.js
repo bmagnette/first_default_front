604795e6b12e9ed274dbf89b47bc7f8e7d1e5d97
Baptiste <baptiste.magnette@gmail.com>
2019-05-08 17:43:56 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './public/css/PublicApp/App.css';
 import RouterExample from "./RouterExample";
 
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+
 class App extends Component {
   render() {
     return (
